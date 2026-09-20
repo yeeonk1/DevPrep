c6e2ea6d4dfbdefd8ce0c65109db0a214f6e6371
yeeonk1 <jyk5152532@gmail.com>
2026-09-20 23:20:29 +0900

    feat: 일정 api 추가

--- frontend/DevPrepFront/src/api/schedule/schedule.ts
+++ frontend/DevPrepFront/src/api/schedule/schedule.ts
@@ -9,8 +9,8 @@ const api = axios.create({
 });
 
 export const scheduleApi = {
-  // 일정 조회
-  getSchedule: async (
+  // 일정 조회 (달력)
+  getScheduleByCalendar: async (
     userId: string,
     startAt: Date,
     endAt: Date,
@@ -19,6 +19,26 @@ export const scheduleApi = {
       params: { userId, startAt, endAt },
     });
 
+    return res.data;
+  },
+
+  // 일정 조회 (오늘/내일 목록)
+  getTodayAndTomorrowSchedule: async (
+    userId: string,
+  ): Promise<ScheduleResponse> => {
+    const res = await api.get("/list", {
+      params: { userId },
+    });
+
+    return res.data;
+  },
+
+  // 일정 조회 (상세)
+  getScheduleDetail: async (
+    userId: string,
+    scheduleId: BigInt,
+  ): Promise<ScheduleResponse> => {
+    const res = await api.get("/detail", { params: { userId, scheduleId } });
     return res.data;
   },
 
